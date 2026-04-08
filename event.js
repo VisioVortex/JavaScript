d2796040bd62d936b04e4bd94d3b6fed5f37196b
VisioVortex <mdmainulislamrabby5@gmail.com>
2026-04-08 23:05:54 +0600

    learned event

--- event.js
+++ event.js
@@ -1,11 +1,24 @@
 // ()
 // #
-let btn = document.querySelector("#btn");
-btn.onclick = () =>{
-    console.log("One Click at a time");
-}
+// let btn = document.querySelector("#btn");
+// btn.onclick = () =>{
+//     console.log("One Click at a time");
+// }
 
-btn.onmouseover = (evt) =>{
-    console.log("Your Mouse");
-    console.log(evt);
-}
+// btn.onmouseover = (evt) =>{
+//     console.log("Your Mouse");
+//     console.log(evt);
+// }
+
+let modeBtn = document.querySelector("#btn");
+let currMode = "light";
+modeBtn.addEventListener("click", () => {
+    if(currMode === "light"){
+        currMode = "dark";
+        document.querySelector("body").style.backgroundColor = "black";
+    } else{
+        currMode = "light"
+        document.querySelector("body").style.backgroundColor = "white";
+    }
+    console.log(currMode);
+});
